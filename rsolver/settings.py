f515ac00979fd55f5eceee94d6f9c99bd9c0aeee
James A. Cox <jacox@jacox.net>
2022-04-16 09:27:07 -0500

    heroku integration

--- rsolver/settings.py
+++ rsolver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_meze8laga=g5_wyxxr&=6*7u)k^g3#vo!se%qkjma=pbv@bv^'
+# SECRET_KEY = 'django-insecure-_meze8laga=g5_wyxxr&=6*7u)k^g3#vo!se%qkjma=pbv@bv^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
