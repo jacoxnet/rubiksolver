b60405f4487a57bcd3d0d1d4fe602aa2fff006ba
James A. Cox <jacox@jacox.net>
2022-04-16 11:32:49 -0500

    heroku

--- rsolver/settings.py
+++ rsolver/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -21,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-_meze8laga=g5_wyxxr&=6*7u)k^g3#vo!se%qkjma=pbv@bv^'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_meze8laga=g5_wyxxr&=6*7u)k^g3#vo!se%qkjma=pbv@bv^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [['0.0.0.0', 'localhost', '127.0.0.1', 'rsolver-app.herokuapp.com']]
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -76,13 +78,26 @@ WSGI_APPLICATION = 'rsolver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wsolver',
+        'USER': 'jacox',
+        'PASSWORD': 'db',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
